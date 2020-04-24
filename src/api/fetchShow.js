68616c216b22e817dc9ac27cca99a5df5ba48ef7
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-04-23 22:04:26 -0700

    mvp

---
+++ src/api/fetchShow.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import axios from 'axios'
+
+const fetchShow = () => {
+      return axios
+        .get(
+          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
+        .then(res => {
+        	return res
+          // setShow(res.data);
+          // setSeasons(formatSeasons(res.data._embedded.episodes));
+        });
+    }
+
+    export default fetchShow
